48e0ddc6ae61a6b1ec822a070572ca3f5fb608b4
zeozez <67858038+zeozez@users.noreply.github.com>
2022-10-03 14:49:13 +0000

    Misc code cleanup

--- example/DataReader.py
+++ example/DataReader.py
@@ -4,3 +4,7 @@ A data parser for Porto Seguro's Safe Driver Prediction competition's dataset.
 URL: https://www.kaggle.com/c/porto-seguro-safe-driver-prediction
 """
 import pandas as pd
+
+
+class FeatureDictionary(object):
+    def __init__(self, trainfile=None, testfile=None,
