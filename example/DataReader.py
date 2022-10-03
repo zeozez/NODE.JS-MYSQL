64720678ce59da053807db47969788b351b420e0
zeozez <67858038+zeozez@users.noreply.github.com>
2022-10-03 18:07:19 +0000

    [doc] update readme

--- example/DataReader.py
+++ example/DataReader.py
@@ -9,3 +9,6 @@ import pandas as pd
 class FeatureDictionary(object):
     def __init__(self, trainfile=None, testfile=None,
                  dfTrain=None, dfTest=None, numeric_cols=[], ignore_cols=[]):
+        assert not ((trainfile is None) and (dfTrain is None)), "trainfile or dfTrain at least one is set"
+        assert not ((trainfile is not None) and (dfTrain is not None)), "only one can be set"
+        assert not ((testfile is None) and (dfTest is None)), "testfile or dfTest at least one is set"
