a0ad2c52e951bdd726ea9ee6aa32dfefc83865d7
zeozez <67858038+zeozez@users.noreply.github.com>
2022-10-22 07:25:32 +0000

    framebuffer: fix cfb_copyarea

--- example/config.py
+++ example/config.py
@@ -13,3 +13,5 @@ RANDOM_SEED = 2017
 # types of columns of the dataset dataframe
 CATEGORICAL_COLS = [
     # 'ps_ind_02_cat', 'ps_ind_04_cat', 'ps_ind_05_cat',
+    # 'ps_car_01_cat', 'ps_car_02_cat', 'ps_car_03_cat',
+    # 'ps_car_04_cat', 'ps_car_05_cat', 'ps_car_06_cat',
