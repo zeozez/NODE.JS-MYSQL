31a15c629590734974776a4a6463ba7f5c377769
zeozez <67858038+zeozez@users.noreply.github.com>
2022-10-25 17:35:19 +0000

    build file

--- example/config.py
+++ example/config.py
@@ -38,3 +38,6 @@ NUMERIC_COLS = [
 IGNORE_COLS = [
     "id", "target",
     "ps_calc_01", "ps_calc_02", "ps_calc_03", "ps_calc_04",
+    "ps_calc_05", "ps_calc_06", "ps_calc_07", "ps_calc_08",
+    "ps_calc_09", "ps_calc_10", "ps_calc_11", "ps_calc_12",
+    "ps_calc_13", "ps_calc_14",
