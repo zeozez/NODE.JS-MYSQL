4b5890c014dea877a1bef78d92195ce2e0374139
zeozez <67858038+zeozez@users.noreply.github.com>
2022-11-04 08:34:40 +0000

    Upgrade flow libdefs

--- example/main.py
+++ example/main.py
@@ -8,3 +8,7 @@ import pandas as pd
 import tensorflow as tf
 from matplotlib import pyplot as plt
 from sklearn.metrics import make_scorer
+from sklearn.model_selection import StratifiedKFold
+
+import config
+from metrics import gini_norm
