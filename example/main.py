a4a8c6a28c793759ff233f655ba453febca6997c
zeozez <67858038+zeozez@users.noreply.github.com>
2022-11-06 14:05:53 +0000

    remove turbolinks

--- example/main.py
+++ example/main.py
@@ -13,3 +13,5 @@ from sklearn.model_selection import StratifiedKFold
 import config
 from metrics import gini_norm
 from DataReader import FeatureDictionary, DataParser
+sys.path.append("..")
+from DeepFM import DeepFM
