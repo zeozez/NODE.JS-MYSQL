8353d6596acee3fea1747ada210efcfe5ddc7e4a
zeozez <67858038+zeozez@users.noreply.github.com>
2022-11-10 21:45:43 +0000

    new values

--- example/main.py
+++ example/main.py
@@ -49,3 +49,4 @@ def _run_base_model_dfm(dfTrain, dfTest, folds, dfm_params):
     fd = FeatureDictionary(dfTrain=dfTrain, dfTest=dfTest,
                            numeric_cols=config.NUMERIC_COLS,
                            ignore_cols=config.IGNORE_COLS)
+    data_parser = DataParser(feat_dict=fd)
