efae3d91724faf028305b0e8369f344bde9d8d18
zeozez <67858038+zeozez@users.noreply.github.com>
2022-11-10 17:24:30 +0000

    add copyright

--- example/main.py
+++ example/main.py
@@ -43,3 +43,4 @@ def _load_data():
     cat_features_indices = [i for i,c in enumerate(cols) if c in config.CATEGORICAL_COLS]
 
     return dfTrain, dfTest, X_train, y_train, X_test, ids_test, cat_features_indices
+
