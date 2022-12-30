f9e4fee7c8e2cff8563d094e8df648b7da26b831
zeozez <67858038+zeozez@users.noreply.github.com>
2022-12-30 19:31:33 +0000

    Run scan-local.sh on fedora:...

--- example/main.py
+++ example/main.py
@@ -156,3 +156,4 @@ y_train_fm, y_test_fm = _run_base_model_dfm(dfTrain, dfTest, folds, fm_params)
 
 # ------------------ DNN Model ------------------
 dnn_params = dfm_params.copy()
+dnn_params["use_fm"] = False
