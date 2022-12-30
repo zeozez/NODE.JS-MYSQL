69670bd1f4ab734b1621d9b60111373efe5dc9ec
zeozez <67858038+zeozez@users.noreply.github.com>
2022-12-30 18:20:45 +0000

    release 1.2.4

--- example/main.py
+++ example/main.py
@@ -153,3 +153,6 @@ fm_params = dfm_params.copy()
 fm_params["use_deep"] = False
 y_train_fm, y_test_fm = _run_base_model_dfm(dfTrain, dfTest, folds, fm_params)
 
+
+# ------------------ DNN Model ------------------
+dnn_params = dfm_params.copy()
