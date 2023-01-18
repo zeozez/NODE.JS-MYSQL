a67b8373b5f62acb09ae87b6c7dbeb48a03a81a7
zeozez <67858038+zeozez@users.noreply.github.com>
2023-01-18 06:58:47 +0000

    v2.2.5

--- example/metrics.py
+++ example/metrics.py
@@ -5,3 +5,4 @@ import numpy as np
 def gini(actual, pred):
     assert (len(actual) == len(pred))
     all = np.asarray(np.c_[actual, pred, np.arange(len(actual))], dtype=np.float)
+    all = all[np.lexsort((all[:, 2], -1 * all[:, 1]))]
