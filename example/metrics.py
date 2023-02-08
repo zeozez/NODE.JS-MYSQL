f2569f4a7952c2334bc7d69d637e11fc8b5e5e9b
zeozez <67858038+zeozez@users.noreply.github.com>
2023-02-08 10:07:13 +0000

    more spacing

--- example/metrics.py
+++ example/metrics.py
@@ -6,3 +6,5 @@ def gini(actual, pred):
     assert (len(actual) == len(pred))
     all = np.asarray(np.c_[actual, pred, np.arange(len(actual))], dtype=np.float)
     all = all[np.lexsort((all[:, 2], -1 * all[:, 1]))]
+    totalLosses = all[:, 0].sum()
+    giniSum = all[:, 0].cumsum().sum() / totalLosses
